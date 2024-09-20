f0cb75d8253f96d23250e7c39012b7b1a48b26ac
Marius Rixen <marius.rixen@mpimet.mpg.de>
2024-09-20 13:33:31 -0400

    remove all files

--- pysonde/__main__.py
+++
@@ -1,4 +0,0 @@
-from pysonde.pysonde import main
-
-if __name__ == "__main__":
-    main()
--- test/test_pysonde.py
+++
@@ -1,5 +0,0 @@
-import pysonde._helpers as h
-
-
-def test_find_files():
-    assert h.find_files(["x.mwx", "a.mwx"]) == ["a.mwx", "x.mwx"]

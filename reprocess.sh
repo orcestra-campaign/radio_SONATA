41cbf201f94f8634f9e91f88d228d72c5dafb8ca
Marius Rixen <marius.rixen@mpimet.mpg.de>
2024-09-20 14:04:33 -0400

    add example

--- reprocess.sh
+++ reprocess.sh
@@ -3,7 +3,7 @@
 sounding_converter -i ./examples/level0/*.mwx -o ./examples/level1_mwx/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc -c config/main_mwx.yaml
 sounding_converter -i ./examples/level0/*.cor -o ./examples/level1_cor/RS_{campaign}_{platform}_L1_%Y%m%dT%H%M_{direction}.nc -c config/main_cor.yaml
 
-#Add the reformatting code here:
+#Add the reformatting code here
 
 
 
